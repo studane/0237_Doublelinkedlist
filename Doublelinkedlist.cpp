9cb59fd41bd3a548ab4ba6b23939829a9641390e
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-28 12:58:30 +0700

    menambah menu

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -213,3 +213,23 @@ public:
     }
 };
 
+int main()
+{
+    DoubleLinkedList list;
+    char choice;
+
+    do
+    {
+
+        cout << "\nMenu:\n";
+        cout << "1. Add Record\n";
+        cout << "2. Delete Record\n";
+        cout << "3. View Ascending\n";
+        cout << "4. View Descending\n";
+        cout << "5. Search Record\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        
