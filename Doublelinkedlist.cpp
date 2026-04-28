5a5804eead48b8785b9af4fdddaf134703a1b82e
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-28 12:58:53 +0700

    menambah case

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -232,4 +232,26 @@ int main()
 
         cin >> choice;
 
-        
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "Invalid option\n";
+        }
+ 
