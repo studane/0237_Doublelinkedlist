b3e04189e8269afbc8485306996e1e3db3fd838c
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-28 12:53:57 +0700

    menghapus node tengah dan belakang

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -113,4 +113,19 @@ public:
             if (START != NULL)
                 START->prev = NULL; // Step 2b: START.prev = NULL
         }
-        
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: If current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
+    }
+
+    
